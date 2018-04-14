77e3b5aa9bd03994ba218acfbb9c968e12542407
Moses Frost <moses@moses.io>
2018-04-13 22:53:09 -0400

    This is an enhancements to a few scripts

--- LIVE/bloodhound-walkthrough.md
+++ LIVE/bloodhound-walkthrough.md
@@ -1,4 +1,25 @@
-Step 0 - 
+Step 1 - Get on Jumphost
+Step 2 - Open Putty - Kali
+Step 3 - Kali run this command:
+
+     sudo apt update -y && sudo apt dist-upgrade -y && sudo apt install bloodhound -y && sudo apt autoremove -y && sudo reboot
+
+Step 4 - On kali once back in run this:
+     cd /opt/empire
+     git pull
+     cd setup
+     ./install.sh
+     
+Step 4 - Back on Jumphost open putty
+Step 5 - Save the following session:
+     198.18.133.5
+     Name: Infra.attack.com
+
+
+
+Step 0 - Add infra.attacker.com to Putty Session on main Jumphost
+Step 0a - 
+
 Step 1 - Add new DNS Pointer to 'safe' biomedical DNS which points to 'Load Balancer' simulating Domain Fronting
 
 Step 2 - Add new Front Page for the SPA for biomed domain called www.happybio.com
--- LIVE/self-signed-cert-gen.sh
+++ LIVE/self-signed-cert-gen.sh
@@ -26,7 +26,11 @@ emailAddress=
 # in case it doesn't exist
 sudo mkdir -p "$SSL_DIR"
 
+# Generate our CA first
+sudo openssl genrsa -out "$SSL_DIR/rootCA.key" 2048
+openssl req -x509 -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -nodes -key "$SSL_DIR/rootCA.key" -sha256 -days 10240 -out "$SSL_DIR/rootCA.pem"
 # Generate our Private Key, CSR and Certificate
 sudo openssl genrsa -out "$SSL_DIR/publiccloud.co.key" 2048
 sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/publiccloud.co.key" -out "$SSL_DIR/publiccloud.co.csr" -passin pass:$PASSPHRASE
-sudo openssl x509 -req -days 3650 -in "$SSL_DIR/publiccloud.co.csr" -signkey "$SSL_DIR/publiccloud.co.key" -out "$SSL_DIR/publiccloud.co.crt"
+#sudo openssl x509 -req -days 3650 -in "$SSL_DIR/publiccloud.co.csr" -signkey "$SSL_DIR/publiccloud.co.key" -out "$SSL_DIR/publiccloud.co.crt"
+sudo openssl x509 -req -days 3650 -in "$SSL_DIR/publiccloud.co.csr" -CA "$SSL_DIR/rootCA.pem" -CAkey "$SSL_DIR/rootCA.key" -CAcreateserial -out "$SSL_DIR/publiccloud.co.crt" -sha256
