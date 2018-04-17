a28023f7f9164bfbfaa46b921d0c8ec58a5837a0
Moses Frost <moses@moses.io>
2018-04-17 16:14:54 -0400

    New Integrations for Empire on Desktop

--- LIVE/attacker-desktop/runme.sh
+++ LIVE/attacker-desktop/runme.sh
@@ -6,4 +6,25 @@ dpkg -i kali-archive-keyring_2018.1_all.deb
      
 sudo apt update -y && sudo apt dist-upgrade -y && sudo apt install bloodhound -y && sudo apt autoremove -y
 
-cat "CTR-3.1" > /etc/ctr-version
+cd /opt/empire
+git pull
+cd /opt/empire/setup/
+./install.sh
+./reset.sh
+
+#Now for the manual crap!
+
+cd /opt/empire
+wget https://github.com/mosesrenegade/cyber-threat-response-clinic/tree/master/LIVE/attacker-desktop/empire.rc
+./empire -r empire.rc
+
+
+echo "CTR-3.1" > /etc/ctr-version
+
+echo "Perform the following steps"
+echo "cd /opt/empire"
+echo "./empire"
+echo "Once empire loads you need to create a http listener and a stager that is launcher_bat"
+echo "move this over to http://198.18.133.5/files/launcher.bat"
+echo "Sorry I can't automate this"
+echo "exeucte the following" 
---
+++ LIVE/runme.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+#This will create a new Jumphost user for bloodhound
