ca5d9fd1e20108fac24c4a2a61e3a75745df8800
Moses Frost <moses@moses.io>
2018-09-19 15:46:34 -0400

    New Fixes

--- servers/oob/heimdal/seeds.py
+++ servers/oob/heimdal/seeds.py
@@ -1,6 +1,7 @@
 import json
+import os
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
+client = MongoClient(os.environ.get('MONGO_HOST', 27017)
 
 db = client['heimdal']
 collection = db['servers']
