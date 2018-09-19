5ae86bcf7499393f189a4d24c61084c1818a8f8a
Moses Frost <moses@moses.io>
2018-09-19 10:26:27 -0400

    Fixed some scripting issues

--- servers/oob/heimdal/tasks.py
+++ servers/oob/heimdal/tasks.py
@@ -2,13 +2,16 @@ import celery
 from celery.signals import task_postrun
 from celery.utils.log import get_task_logger
 import requests
+import os
 
 logger = get_task_logger(__name__)
 
 @celery.task(name="put_to_api")
 def put_to_api(session_id):
-    print(session_id)    
-    r = requests.put(SB_HB_URL + '/' + session_id)
+    #f=open("/heimdal/sess_id.txt",'w')
+    #session_id=f.readlines()
+    print(session_id)
+    r = requests.put(os.environ.get('SB_HB_URL') + '/' + session_id)
 
 @celery.task
 def log(message):
