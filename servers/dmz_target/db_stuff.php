a8e6f191dc0f4b3323c1f991c8296da16fdb7e2a
Moses Frost <moses@moses.io>
2019-06-01 19:40:35 -0400

    New Script to create the Billing Database automatically using safer PDO methods

--- servers/dmz_target/db_stuff.php
+++ servers/dmz_target/db_stuff.php
@@ -2,7 +2,8 @@
 $servername = "localhost";
 $username = "root";
 $password = "C1sco12345";
-$db = "INVOICES"
+$db = "INVOICES";
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     // setting the PDO error mode to exception
@@ -23,7 +24,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $table = "invoices";
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql ="CREATE TABLE IF NOT EXISTS invoices ( id INT AUTO_INCREMENT, 
+    $sql ="CREATE TABLE IF NOT EXISTS $table ( id INT AUTO_INCREMENT, 
            invoice_number VARCHAR(255) NOT NULL , amount_due VARCHAR(255) NOT NULL, 
            date VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, 
            lastname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, 
@@ -32,6 +33,19 @@ try {
     $conn->exec($sql);
      print("Created $table Table.\n");
 
+} catch(PDOException $e) {
+    echo $e->getMessage();//Remove or change message in production code
+}
+
+try {
+    $table = "invoices";
+    $sql = "INSERT INTO $table (invoice_number, amount_due, date, 
+            firstname, lastname, email, address, cc_num) VALUES 
+            ('000000001', '388.00', '5/1/2019', 'DOOGIE', 'HOUSER', 
+            'DaMan1990@aol.com', '1428 Elm Street', 
+            '5145757998170252');";
+    $conn->exec($sql);
+    print("Created $table Table.\n");
 } catch(PDOException $e) {
     echo $e->getMessage();//Remove or change message in production code
 }
