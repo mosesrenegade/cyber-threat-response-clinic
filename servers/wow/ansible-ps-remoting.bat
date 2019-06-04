fd7d6593be57e4486d0f86f8a10a58f226b50906
Moses Frost <moses@moses.io>
2019-06-04 09:09:17 -0400

    .NET Framework stuff

--- servers/wow/ansible-ps-remoting.bat
+++ servers/wow/ansible-ps-remoting.bat
@@ -2,3 +2,6 @@
 sc.exe config wuauserv start= demand
 net start wuauserv
 .\Win7AndW2K8R2-KB3191566-x64.msu
+.\NDP452-KB2901907-x86-x64-AllOS-ENU.exe
+net stop wuauserv
+sc.exe config wuauserv start= disabled
