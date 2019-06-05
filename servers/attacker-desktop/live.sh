9d6c2fd1d6d4bfcfce944287f08e2990a074d460
Moses Frost <moses@moses.io>
2019-06-04 23:11:09 -0400

    BloodHound

--- servers/attacker-desktop/live.sh
+++ servers/attacker-desktop/live.sh
@@ -2,6 +2,8 @@
 wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/servers/attacker-desktop/Invoke-Mimikatz.ps1 -O /tmp/Invoke-Mimikatz.ps1
 wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/servers/attacker-desktop/safe.bat -O /tmp/safe.bat
 wget https://raw.githubusercontent.com/mosesrenegade/cyber-threat-response-clinic/master/servers/attacker-desktop/SharpHound.ps1 -O /tmp/SharpHound.ps1
+wget https://github.com/mosesrenegade/cyber-threat-response-clinic/raw/master/servers/attacker-desktop/BloodHound-linux-x64.zip -O /tmp/BloodHound-linux-x64.zip
+
 cd /tmp
 python -m 'SimpleHTTPServer' 9001
 
