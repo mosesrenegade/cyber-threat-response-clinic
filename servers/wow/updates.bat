53bafabf1f1e28d203f20722f5257117df4d3d65
Moses Frost <moses@moses.io>
2019-06-09 15:58:30 -0700

    Broken f*ing WIndows update

--- servers/wow/updates.bat
+++ servers/wow/updates.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 sc.exe config wuauserv start= demand
 net start wuauserv
-wuau.exe .\Windows6.1-KB3033929-x64.msu /quiet
-wuau.exe .\Win7AndW2K8R2-KB3191566-x64.msu /quiet
+wusa.exe .\Windows6.1-KB3033929-x64.msu /quiet
+wusa.exe .\Win7AndW2K8R2-KB3191566-x64.msu /quiet
 .\NDP452-KB2901907-x86-x64-AllOS-ENU.exe /q /norestart
 net stop wuauserv
 sc.exe config wuauserv start= disabled
